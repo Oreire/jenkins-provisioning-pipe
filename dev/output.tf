c75ae17bbbda9a248ad9285b002031d3b636a3c0
Ayomide Ajayi <lancerajayi@yahoo.com>
2024-11-22 17:48:41 +0000

    jenkinsfile update

--- dev/output.tf
+++ dev/output.tf
@@ -2,14 +2,14 @@ output "Nginx" {
   value = aws_instance.node1.public_dns
 }
 
-output "Nginx_IP" {
-  value = aws_instance.node1.public_ip
-}
+# output "Nginx_IP" {
+#   value = aws_instance.node1.public_ip
+# }
 
 output "Pynode" {
   value = aws_instance.node2.public_dns
 }
 
-output "Pynode_IP" {
-  value = aws_instance.node2.public_ip
-}
+# output "Pynode_IP" {
+#   value = aws_instance.node2.public_ip
+# }
